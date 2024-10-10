52d3f93afb6021982919f0636c9d4214e9d11949
Pavel.Notebook <pas-zhukov@yandex.ru>
2024-10-10 12:32:57 +0300

    initial

---
+++ src/main/java/ru/zhukov/usersandpets/UsersAndPetsApplication.java
@@ -0,0 +1,13 @@
+package ru.zhukov.usersandpets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsersAndPetsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UsersAndPetsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/zhukov/usersandpets/UsersAndPetsApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.zhukov.usersandpets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsersAndPetsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
